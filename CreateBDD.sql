8d38a91f9c3f59002bc9a595831bbd1fd1fac79d
GrooZey <theiss.sean@gmail.com>
2023-08-31 15:20:01 +0200

    modif CreateBDD.sql

--- CreateBDD.sql
+++ CreateBDD.sql
@@ -8,19 +8,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema cloudy
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema cloudy
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `cloudy` DEFAULT CHARACTER SET utf8 ;
+USE `cloudy` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Role`
+-- Table `cloudy`.`Role`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Role` (
+CREATE TABLE IF NOT EXISTS `cloudy`.`Role` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`))
@@ -28,9 +28,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`User`
+-- Table `cloudy`.`User`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`User` (
+CREATE TABLE IF NOT EXISTS `cloudy`.`User` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `email` VARCHAR(100) NOT NULL,
@@ -41,16 +41,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`User` (
   INDEX `fk_User_Role_idx` (`Role_id` ASC) VISIBLE,
   CONSTRAINT `fk_User_Role`
     FOREIGN KEY (`Role_id`)
-    REFERENCES `mydb`.`Role` (`id`)
+    REFERENCES `cloudy`.`Role` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Theme`
+-- Table `cloudy`.`Theme`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Theme` (
+CREATE TABLE IF NOT EXISTS `cloudy`.`Theme` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`))
@@ -58,9 +58,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Picture`
+-- Table `cloudy`.`Picture`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Picture` (
+CREATE TABLE IF NOT EXISTS `cloudy`.`Picture` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(45) NOT NULL,
   `description` TEXT NULL,
@@ -73,21 +73,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Picture` (
   INDEX `fk_Picture_Theme1_idx` (`Theme_id` ASC) VISIBLE,
   CONSTRAINT `fk_Picture_User1`
     FOREIGN KEY (`User_id`)
-    REFERENCES `mydb`.`User` (`id`)
+    REFERENCES `cloudy`.`User` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Picture_Theme1`
     FOREIGN KEY (`Theme_id`)
-    REFERENCES `mydb`.`Theme` (`id`)
+    REFERENCES `cloudy`.`Theme` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Favorite`
+-- Table `cloudy`.`Favorite`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Favorite` (
+CREATE TABLE IF NOT EXISTS `cloudy`.`Favorite` (
   `Picture_id` INT NOT NULL,
   `User_id` INT NOT NULL,
   PRIMARY KEY (`Picture_id`, `User_id`),
@@ -95,12 +95,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Favorite` (
   INDEX `fk_Picture_has_User_Picture1_idx` (`Picture_id` ASC) VISIBLE,
   CONSTRAINT `fk_Picture_has_User_Picture1`
     FOREIGN KEY (`Picture_id`)
-    REFERENCES `mydb`.`Picture` (`id`)
+    REFERENCES `cloudy`.`Picture` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Picture_has_User_User1`
     FOREIGN KEY (`User_id`)
-    REFERENCES `mydb`.`User` (`id`)
+    REFERENCES `cloudy`.`User` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
