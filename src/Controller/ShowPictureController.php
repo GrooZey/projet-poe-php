e35d2a9114cba07b2c0fec7a59be06395cbdc2b0
GrooZey <theiss.sean@gmail.com>
2023-09-28 10:37:43 +0200

    dropdown menu v.desctop

--- src/Controller/ShowPictureController.php
+++ src/Controller/ShowPictureController.php
@@ -5,15 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Picture;
 
 #[Route('/picture', name: 'app_picture_')]
 class ShowPictureController extends AbstractController
 {
-    #[Route('/show', name: 'show')]
-    public function show(): Response
+    #[Route('/show/{id}', name: 'show')]
+    public function show(EntityManagerInterface $entityManager, int $id): Response
     {
+        $pic = $entityManager->getRepository(Picture::class)->find($id);
+        if (!$pic) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
         return $this->render('show/index.html.twig', [
+            'picture' => $pic,
         ]);
     }
 }
