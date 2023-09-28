68b750f88d2477d5f6827b72cb243d197cda7bcf
Nico-Epitech <nicolas.leveque@epitech.eu>
2023-09-28 11:30:49 +0200

    Correctifs templates base & home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Picture;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('home/index.html.twig');
+        $repository = $entityManager->getRepository(Picture::class);
+        $pictures = $repository->findAll();
+        return $this->render('home/index.html.twig',[
+            'pictures' => $pictures,
+        ]);
     }
 }
